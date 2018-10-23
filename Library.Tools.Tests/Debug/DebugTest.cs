5e1f14feecabf7b840af9b68a798121d9b288629
jmalbault <jmalbault@lecapitaine.fr>
2018-10-23 15:50:12 +0200

    Ajout debug test dans git

---
+++ Library.Tools.Tests/Debug/DebugTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library.Tools.Debug;
+
+namespace Library.Tools.Tests
+{
+    [TestClass]
+    public class DebugTest
+    {
+        [TestMethod]
+        public void PrintInformation()
+        {
+            MyDebug.PrintInformation("test");
+
+
+
+        }
+    }
+}
